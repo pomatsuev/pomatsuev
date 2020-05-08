8942cb41177a8b28dcbb3f3f252ea266370b2c38
Stanislav Pomatsuev <pomatsuev@gmail.com>
2020-05-08 21:00:20 +1000

    create my site

---
+++ precache-manifest.762ce9c4a4a81eb6ed9a88b257344de0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ef328709daa249a35dcb70689d3164",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1765a1b566a464570031",
+    "url": "/static/css/main.ba371734.chunk.css"
+  },
+  {
+    "revision": "0ed8af781274faf2cdb1",
+    "url": "/static/js/2.3d815fe0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3d815fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1765a1b566a464570031",
+    "url": "/static/js/main.9d26615e.chunk.js"
+  },
+  {
+    "revision": "03d629f59f31c619e1c1",
+    "url": "/static/js/runtime-main.34f306bf.js"
+  },
+  {
+    "revision": "5f07661dc3a7506e8ff4afa17e5adb57",
+    "url": "/static/media/11719.5f07661d.ttf"
+  },
+  {
+    "revision": "70b010901cc976ed57c7399ecfa0ad25",
+    "url": "/static/media/HP.70b01090.png"
+  }
+]);
